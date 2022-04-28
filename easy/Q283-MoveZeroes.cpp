4d0d1d4bcb4220118649bf64c5d49afc812975a6
Ryo-Ura <urawaryosuke@gmail.com>
2022-04-28 00:11:24 -0700

    improved performance

--- easy/Q283-MoveZeroes.cpp
+++ easy/Q283-MoveZeroes.cpp
@@ -5,7 +5,11 @@
  *      Runtime: 120 ms, faster than 11.75% of C++ online submissions for Move Zeroes.
  *      Memory Usage: 19.3 MB, less than 58.71% of C++ online submissions for Move Zeroes.
  *      O(n)
- * @version 0.1
+ *  
+ *      not using erase and push_back cuz they are expensive
+ *      Runtime: 22 ms, faster than 85.13% of C++ online submissions for Move Zeroes.
+ *      Memory Usage: 19.1 MB, less than 84.00% of C++ online submissions for Move Zeroes.
+ * @version 1.1
  * @date 2022-04-27
  * 
  * @copyright Copyright (c) 2022
@@ -25,16 +29,19 @@ using namespace std;
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int size = nums.size();
-        for (int i = size - 1; i >= 0; i--)
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
         {
-            if (nums[i] == 0)
+            if(nums[i] == 0)
             {
-                nums.erase(nums.begin() + i);
-                nums.push_back(0);
+                count++;
             }
-            
+            else if (count != 0)
+            {
+                // move item forward and insert 0 at that index
+                nums[i - count] = nums[i];
+                nums[i] = 0;
+            }
         }
-        
     }
 };
