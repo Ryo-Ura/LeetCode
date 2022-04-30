a72b044e26f7d2d1d6d4e2d665ef8fcb152354ca
Ryo-Ura <urawaryosuke@gmail.com>
2022-04-29 23:59:42 -0700

    use unordered_set instead of set. improved by 2%

--- medium/Q3-LongestSubstringWithoutRepeatingCharacters.cpp
+++ medium/Q3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -4,7 +4,11 @@
  * @brief Runtime: 1394 ms, faster than 5.01% of C++ online submissions for Longest Substring Without Repeating Characters.
  *        Memory Usage: 262.3 MB, less than 5.37% of C++ online submissions for Longest Substring Without Repeating Characters.
  *        O(n)
- * @version 0.1
+ *  
+ *        using unordered_set
+ *        Runtime: 964 ms, faster than 6.95% of C++ online submissions for Longest Substring Without Repeating Characters.
+ *        Memory Usage: 149.1 MB, less than 7.54% of C++ online submissions for Longest Substring Without Repeating Characters.
+ * @version 1.1
  * @date 2022-04-29
  * 
  * @copyright Copyright (c) 2022
@@ -19,6 +23,8 @@
 #include <map>
 #include <math.h>
 #include <utility>
+#include <unordered_set>
+
 
 using namespace std;
 class Solution {
@@ -27,7 +33,7 @@ public:
         int currSize = 0;
         int max = 0;
         int temp = 0;
-        set<char> mySet;
+        unordered_set<char> mySet;
         for (int i = 0; i < s.size(); i++)
         {
             if (!mySet.insert(s[i]).second)
